1870029b0c1045beb049ef337c2a9856731c9876
tatianosantos <tatianosantos825@gmail.com>
2024-07-29 16:53:52 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+
+while True:
+    num1 = input("digite num1: ")
+    num2 = input("digite num2: ")
+    num1 = int(num1)
+    num2 = int(num2)
+
+    soma = num1 + num2
+    print("soma: ", soma)
+    sub = num1-num2
+    print("sub: ", sub)
